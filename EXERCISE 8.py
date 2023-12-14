13fec1e940f91bce2f5279bceac2c0ee5c0d33de
notKrunch <113130368+notKrunch@users.noreply.github.com>
2023-12-14 14:20:02 +0400

    Delete EXERCISE 8.py

--- EXERCISE 8.py
+++
@@ -1,8 +0,0 @@
-#range of numbers from 1 - 5
-for i in range(1, 6):
-    # repeats the value from 1 to the new value 
-    for j in range(1, i + 1):
-        # is used to print the numbers on the same line 
-        print(j, end=" ")
-    #this is added to print to the next line below
-    print()
